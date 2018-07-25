c8cfe974717ae49f5288b1a5715de5bb11514f6f
Cristina Lugo <cmlugoce@syr.edu>
2018-07-25 15:50:53 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,7 +9,7 @@ class StudentsController < ApplicationController
     @student = Student.find_by_id(params[:id])
   end
 
-  def active
+  def activate
     @student = set_student
     @student.active = !@student.active
     @student.save
